b2ca783cf5d0f618224ccf457694c7cad304fe4a
Bianca Sarzi <biancasarzidev@gmail.com>
2025-02-06 15:55:14 -0300

    .

--- classes/Player.js
+++ classes/Player.js
@@ -1,12 +1,22 @@
 class Player {
-    constructor() {
+    constructor(canvaswidth, canvasheight) {
         this.width = 100
         this.height = 100
         this.position = {
-            x: 0,
-            y: 0,
+            x: canvaswidth / 2 - this.width / 2,
+            y: canvasheight - this.height - 30,
         }
-    }      
+    }
+    
+    draw(ctx) {
+        ctx.fillStyle = "red";
+        ctx.fillRect(
+            this.position.x, 
+            this.position.y, 
+            this.width, 
+            this.height   
+        );
+    }
 }
 
 export default Player;
--- src/index.js
+++ src/index.js
@@ -6,7 +6,26 @@ const ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-const Player = new Player();
+const player = new Player(canvas.width, canvas.height);
 
-ctx.fillStyle = "red";
-ctx.fillRect(Player.position.x, Player.position.y, Player.width, Player.height);
+
+const gameLoop = () => {
+    player.draw(ctx);
+
+    requestAnimationFrame(gameLoop)
+}
+
+gameLoop();
+
+
+addEventListener("keydown", (event) => {
+    const key = event.key.toLowerCase();
+
+    if (key === "a") {
+        player.position.x -= 20;
+    }
+
+    if (key == "d") {
+        player.positision.x += 20;
+    }
+})
