577cb77ac324ea5b2d4d257ba64d884e017523f1
Bianca Sarzi <biancasarzidev@gmail.com>
2025-02-10 10:55:39 -0300

    .

--- utils/constant.js
+++ utils/constant.js
@@ -1,5 +1,13 @@
 export const PATH_SPACESHIP_IMAGE = "imagens/spaceship.png";
 export const PATH_ENGINE_IMAGE = "imagens/engine.png";
 export const PATH_ENGINE_SPRITES = "imagens/engine_sprites.png";
-export const INITIAL_FRAMES = 8;
 export const PATH_INVADER_IMAGE = "imagens/invader.png";
+
+export const NUMBER_STARS = 100;
+export const INITIAL_FRAMES = 8;
+
+export const GameState = {
+    START: "start",
+    PLAYING: "playing",
+    GAME_OVER: "gameOver",
+};
